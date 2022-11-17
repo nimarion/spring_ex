5b8f546228569b5cb3be430ef3100daec354b331
Niklas Marion <niklas.marion@gmx.de>
2022-11-17 12:35:10 +0100

    change swagger base

--- src/main/java/de/nimarion/spring/Application.java
+++ src/main/java/de/nimarion/spring/Application.java
@@ -3,6 +3,9 @@ package de.nimarion.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class Application {
 
